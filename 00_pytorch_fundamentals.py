b76d71793148a30e92c36418ca1ed9df9d1acdaf
Travis Wessman <traviswessman26@gmail.com>
2026-08-26 09:34:21 -0400

    messing around with tensors

--- 00_pytorch_fundamentals.py
+++ 00_pytorch_fundamentals.py
@@ -2,7 +2,15 @@ import torch
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 print(torch.__version__)
+
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print (x)
+else:
+    print ("MPS device not found.")
 
 ## Introduction to Tensors
 
@@ -29,3 +37,15 @@ print(vector.ndim)
 # matrix
 MATRIX = torch.tensor([[7, 6],
                        [5, 6]])
+print(MATRIX)
+
+print(MATRIX.ndim)
+print(MATRIX.shape)
+
+# TENSOR
+TENSOR = torch.tensor([[[1, 2, 3, 4],
+                        [2, 3, 4, 2],
+                        [2, 3, 1, 0]]])
+print(TENSOR)
+print(TENSOR.ndim)
+print(TENSOR.shape)
